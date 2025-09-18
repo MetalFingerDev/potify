45a65451db6ca3b899c955b6640d3b06906d716a
Metal Mask <dev.prajwalp@gmail.com>
2025-09-18 16:23:44 +0530

    green font is ugly

--- components/SidebarItem.tsx
+++ components/SidebarItem.tsx
@@ -3,34 +3,34 @@ import Link from "next/link";
 import { twMerge } from "tailwind-merge";
 
 interface SidebarItemProps {
-  icon: IconType;
-  label: string;
-  activate?: boolean;
-  href: string
+	icon: IconType;
+	label: string;
+	activate?: boolean;
+	href: string;
 }
 
 const SidebarItem: React.FC<SidebarItemProps> = ({
-  icon: Icon,
-  label,
-  active,
-  href
+	icon: Icon,
+	label,
+	active,
+	href,
 }) => {
-  return (
-    <Link
-      href={href}
-      className={twMerge(`
+	return (
+		<Link
+			href={href}
+			className={twMerge(
+				`
           flex
           flex-row
           h-auto
           items-center
         `,
-          active && "text-green-500"
-      )}
-    >
-      <Icon size={26} />
-      <p className="truncate w-full">{label}</p>
-    </Link>
-  );
-}
+				active && "text-green-500"
+			)}>
+			<Icon size={26} />
+			<p className='truncate w-full'>{label}</p>
+		</Link>
+	);
+};
 
 export default SidebarItem;
