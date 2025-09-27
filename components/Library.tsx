9c490563c63e74c8cc26de4351682e3108d5f6ca
Metal Mask <dev.prajwalp@gmail.com>
2025-09-27 11:20:05 +0530

    library layouted

--- components/Library.tsx
+++ components/Library.tsx
@@ -7,26 +7,23 @@ const Library = () => {
 	const onClick = () => {};
 	return (
 		<div className='flex flex-col'>
-			<div className='flex items-center justify-between px-5 pt-4'>
-				<div>
+			<div className='flex items-center justify-between'>
+				<div className='inline-flex items-center gap-x-2'>
 					<TbPlaylist className='text-neutral-400' size={26} />
 					<p className='text-neutral-400 font-medium text-md'>Library</p>
-					<div className='inline-flex items-center gap-x-2'></div>
 				</div>
 				<AiOutlinePlus
 					onClick={onClick}
 					size={20}
 					className='
-            text-neutral-400
-            cursor-pointer
-            hover:text-white
-            transition'
+						text-neutral-400
+						cursor-pointer
+						hover:text-white
+						transition
+						'
 				/>
-				<div
-					className='inline-flex
-          items-center
-          gap-x-2'></div>
 			</div>
+			<div>songs</div>
 		</div>
 	);
 };
